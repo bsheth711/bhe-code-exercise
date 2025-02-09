e6054dce72b56a1bd8d7ffbf3e3fa93a19ac5647
bsheth <bsheth@wisc.edu>
2025-02-09 01:09:30 -0600

    update public/private

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 3. Increasing blockSize * 2 after processing a block
 4. Increasing blockSize as much as possible after processing a block
 5. Honing in on optimal maxBlockSize
-6. Preallocating plenty of space for primes slice
+6. Preallocating plenty of space for primes slice had small improvement
+7. Preallocating space for isNotPrime had no detectable effect surprisingly
 
 # Further Possible Optimizations
 1. Cache line alignment of slices
--- go/pkg/sieve/sieve.go
+++ go/pkg/sieve/sieve.go
@@ -4,28 +4,35 @@ type Sieve interface {
 	NthPrime(n int64) int64
 }
 
-type EratosthenesSieve struct {
-	primes       []int64
+const startingBlockSize = 1 << 9
+
+// Note: this is the maximum block size and heavily impacts performance.
+// May require dialing in based on specifically your hardware, eg. memory, CPU cache sizes
+// Currently, it is set to the optimal value for my M1 Macbook Air: 100000000 take ~12s
+const maxBlockSize = 1 << 24
+
+type eratosthenesSieve struct {
+	Primes       []int64
+	isNotPrime   []bool
 	blockStart   int64
 	blockSize    int64
 	maxBlockSize int64
 }
 
-func (eraSieve EratosthenesSieve) NthPrime(n int64) int64 {
+func (eraSieve eratosthenesSieve) NthPrime(n int64) int64 {
 	if n < 0 {
 		return 2
 	}
 
-	if n < int64(len(eraSieve.primes)) {
-		return eraSieve.primes[n]
+	if n < int64(len(eraSieve.Primes)) {
+		return eraSieve.Primes[n]
 	}
 
-	for int64(len(eraSieve.primes))-1 < n {
+	for int64(len(eraSieve.Primes))-1 < n {
 
 		blockEnd := eraSieve.blockStart + eraSieve.blockSize
-		var isNotPrime []bool = make([]bool, eraSieve.blockSize)
 
-		for _, prime := range eraSieve.primes {
+		for _, prime := range eraSieve.Primes {
 
 			multiplier := eraSieve.blockStart / prime
 
@@ -39,17 +46,19 @@ func (eraSieve EratosthenesSieve) NthPrime(n int64) int64 {
 			for multiple < blockEnd {
 				offset := multiple - eraSieve.blockStart
 
-				isNotPrime[offset] = true
+				eraSieve.isNotPrime[offset] = true
 
 				multiplier++
 				multiple = prime * multiplier
 			}
 		}
 
-		for i := range isNotPrime {
-			if !isNotPrime[i] {
-				number := int64(i) + eraSieve.blockStart
-				eraSieve.primes = append(eraSieve.primes, number)
+		for i := int64(0); i < eraSieve.blockSize; i++ {
+			if !eraSieve.isNotPrime[i] {
+				number := i + eraSieve.blockStart
+				eraSieve.Primes = append(eraSieve.Primes, number)
+			} else {
+				eraSieve.isNotPrime[i] = false
 			}
 		}
 
@@ -64,36 +73,33 @@ func (eraSieve EratosthenesSieve) NthPrime(n int64) int64 {
 		}
 	}
 
-	return eraSieve.primes[n]
+	return eraSieve.Primes[n]
 }
 
 func NewSieve() Sieve {
-	eraSieve := EratosthenesSieve{}
-	eraSieve.primes = make([]int64, 10, 200000001)
+	eraSieve := eratosthenesSieve{}
+	eraSieve.Primes = make([]int64, 10, 100000001)
+	eraSieve.isNotPrime = make([]bool, maxBlockSize)
 
 	// seeding the EratosthenesSieve with precalculated values
 	// so segmenting can be used straight away
-	eraSieve.primes[0] = 2
-	eraSieve.primes[1] = 3
-	eraSieve.primes[2] = 5
-	eraSieve.primes[3] = 7
-	eraSieve.primes[4] = 11
-	eraSieve.primes[5] = 13
-	eraSieve.primes[6] = 17
-	eraSieve.primes[7] = 19
-	eraSieve.primes[8] = 23
-	eraSieve.primes[9] = 29
+	eraSieve.Primes[0] = 2
+	eraSieve.Primes[1] = 3
+	eraSieve.Primes[2] = 5
+	eraSieve.Primes[3] = 7
+	eraSieve.Primes[4] = 11
+	eraSieve.Primes[5] = 13
+	eraSieve.Primes[6] = 17
+	eraSieve.Primes[7] = 19
+	eraSieve.Primes[8] = 23
+	eraSieve.Primes[9] = 29
 
 	// next number to start algorithm at after the last known prime
 	eraSieve.blockStart = 30
 
 	// starting block size, will expand to maxBlockSize
-	eraSieve.blockSize = 1 << 9
-
-	// Note: this is the maximum block size and heavily impacts performance.
-	// May require dialing in based on specifically your hardware, eg. memory, CPU cache sizes
-	// Currently, it is set to the optimal value for my M1 Macbook Air: 100000000 take ~12s
-	eraSieve.maxBlockSize = 1 << 24
+	eraSieve.blockSize = startingBlockSize
+	eraSieve.maxBlockSize = maxBlockSize
 
 	return eraSieve
 }
